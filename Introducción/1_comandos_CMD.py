aac76cdf32d6e2b482b3771cb9c5fa4f6824df1a
Maicol Acuña <maicolacuna196@gmail.com>
2025-09-19 08:59:46 -0500

    add files

---
+++ Introducción/1_comandos_CMD.py
@@ -0,0 +1,21 @@
+"""
+--- 💻 Comandos Básicos de Linux ---
+
+### 📂 Comandos de Navegación y Archivos
+
+* `pwd` ➡️ Muestra la ruta del directorio actual.
+* `ls` ➡️ Lista los archivos y directorios.
+* `cd` ➡️ Cambia de directorio.
+* `mkdir` ➡️ Crea un nuevo directorio.
+* `touch` ➡️ Crea un archivo vacío.
+* `rm` ➡️ Borra archivos y directorios (¡usa con cuidado! ⚠️).
+* `cp` ➡️ Copia archivos.
+* `mv` ➡️ Mueve o renombra un archivo.
+* `cat` ➡️ Muestra el contenido de un archivo.
+
+### 🛠️ Comandos de Utilidad
+
+* `man` ➡️ Muestra el manual de un comando.
+* `clear` ➡️ Limpia la pantalla de la terminal.
+* `exit` ➡️ Cierra la terminal.
+"""
---
+++ Introducción/2_Windows _(WSL) .py
@@ -0,0 +1,40 @@
+"""
+--- 🐍 Comandos de Python y Linux ---
+
+### Comandos Utilizados
+
+* `python` ➡️ Abre la interfaz de Python 2 (en sistemas antiguos).
+* `python3` ➡️ Abre la interfaz de Python 3.
+* `exit()` ➡️ Cierra la interfaz de Python.
+
+---
+
+### Instalación en Linux 🐧
+
+* `apt update` ➡️ Actualiza la lista de paquetes disponibles.
+* `sudo apt update` ➡️ Ejecuta la actualización con permisos de administrador.
+* `sudo apt -y upgrade` ➡️ Actualiza todos los paquetes instalados. La opción `-y` responde 'sí' a todas las preguntas.
+
+---
+
+### Verificación de Python
+
+* `python3 -V` ➡️ Verifica si Python 3 está instalado y muestra la versión.
+
+---
+
+### Gestor de Paquetes de Python (pip)
+
+* `sudo apt install -y python3-pip` ➡️ Instala el gestor de paquetes pip para Python 3.
+* `pip3 -V` ➡️ Verifica la instalación del gestor de paquetes y su versión.
+
+---
+
+### Dependencias para un Entorno Profesional
+
+* `apt install -y build-essential libssl-dev libffi-dev python3-dev` 
+➡️ Instala las herramientas de desarrollo y bibliotecas necesarias para compilar paquetes de Python desde la fuente.
+
+En resumen, esta lista es una guía rápida y esencial de los comandos básicos de terminal que todo desarrollador de Python necesita 
+para configurar y mantener su entorno de trabajo en Linux, desde la instalación de Python hasta la gestión de dependencias. 🚀
+"""
